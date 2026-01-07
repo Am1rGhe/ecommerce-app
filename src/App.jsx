d7f836a18e024e8d36aca2606abf671a5941c3bb
Amir <amiramir2582004@.com>
2026-01-07 01:20:59 -0500

    add the layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-import Header from "./components/common/Header";
+import Layout from "./components/common/Layout";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <Layout>
+      {/* Your page content goes here */}
+      <div style={{ padding: "20px", minHeight: "400px" }}>
+        <h1>Welcome to TRISTAN</h1>
+        <p>Your content goes here...</p>
+      </div>
+    </Layout>
   );
 }
 
