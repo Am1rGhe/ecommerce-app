6d687f565e210281741d5be41f69a47e1578c7f2
Amir <amiramir2582004@.com>
2026-01-09 15:38:00 -0500

    add about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,8 @@ function About() {
     <Layout>
       <div style={{ padding: "40px 50px", minHeight: "400px" }}>
         <h1>About Us</h1>
-        <p>Learn more about TRISTAN</p>
-        {/* About content will go here */}
+        <p>Learn more about AMIR</p>
+        {/* About content  */}
       </div>
     </Layout>
   );
