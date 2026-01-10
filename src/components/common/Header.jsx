5c1165b81644850457f4ad251e67f573004e0ba3
Amir <amiramir2582004@.com>
2026-01-09 23:42:12 -0500

    add the cart responsive desing

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useCart } from "../../context/CartContext";
 import styles from "./Header.module.css";
 
 function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userName, setUserName] = useState("Amir Ghouari");
+  const { getTotalItems } = useCart();
+  const totalItems = getTotalItems();
 
   const getInitials = (name) => {
     return name
@@ -117,7 +120,7 @@ function Header() {
               </svg>
             </button>
             {/* cart button  */}
-            <Link to="/cart" className={styles.iconBtn} aria-label="Cart">
+            <Link to="/cart" className={styles.cartIconContainer} aria-label="Cart">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -132,6 +135,9 @@ function Header() {
                   d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
                 />
               </svg>
+              {totalItems > 0 && (
+                <span className={styles.cartBadge}>{totalItems}</span>
+              )}
             </Link>
           </div>
         </div>
